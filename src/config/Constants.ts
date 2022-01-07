a81bfda705fdfaa0c201c9ad870295f2a4d7175a
saturno1977 <wellingtonsaturnino30@hotmail.com>
2022-01-07 15:59:19 -0300

    Primeiro commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = { 
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECT: 
+    'mongodb+srv://admin:<password>@shortner-url-dio.umxuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
